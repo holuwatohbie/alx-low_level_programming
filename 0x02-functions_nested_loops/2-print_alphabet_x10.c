25ef627153963a152a2fc468d2931a79c6256890
holuwatohbie <ajayiholuwatohbie@gmail.com>
2023-03-21 13:06:28 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,12 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the alphabet 10 times.
- *
- * Return: Always 0 (Success)
- */
-void print_alphabet_x10(void)
+ *  * main - check the code.
+ *   *
+ *    *Return: Always 0.
+ *     */
+int main(void)
 {
-	int a, b;
-
-	for (a = 0; a <= 9; a++)
-	{
-		for (b = 97; b <= 122; b++)
-		{
-			_putchar(b);
-		}
-	_putchar('\n');
-	}
+	print_alphabet_x10();
+	return (0);
 }
