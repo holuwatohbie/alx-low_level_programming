c66ab01c36d4aa3c355ba9f71b073ae3d8d4abed
holuwatohbie <ajayiholuwatohbie@gmail.com>
2023-03-27 14:36:22 -0700

    i can only go one way

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,17 +6,11 @@
  * Return: no return.
  */
 void _puts(char *str)
-{
-	int count = 0;
 
-	while (count >= 0)
+{
+	for (; *str != '\0'; str++)
 	{
-		if (str[count] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		_putchar(str[count]);
-		count++;
+		_putchar(*str);
 	}
+	_putchar('\n');
 }
